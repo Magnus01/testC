b4761e4ddbe26f57eb7d837f8381e27232db99c7
unknown <magnus01lysfjord@gmail.com>
2018-10-16 14:36:49 +0200

    added errors unit and replacec ompleted_unit

--- API/DockerSandbox.js
+++ API/DockerSandbox.js
@@ -243,7 +243,7 @@ DockerSandbox.prototype.execute = function(success)
 
         unit_myC = unit_myC + 1;
 
-        fs.readFile(sandbox.path + sandbox.folder + '/completed_unit', 'utf8', function(err, data) {
+        fs.readFile(sandbox.path + sandbox.folder + '/errors_unit', 'utf8', function(err, data) {
 
             //if file is not available yet and the file interval is not yet up carry on
             if (err && unit_myC < sandbox.timeout_value)
@@ -282,15 +282,23 @@ DockerSandbox.prototype.execute = function(success)
             //if time is up. Save an error message to the data variable
             else
             {
-
+                //Since the time is up, we take the partial output and return it.
+                fs.readFile(sandbox.path + sandbox.folder + '/errors_unit.txt', 'utf8', function(err, data){
+                    if (!data) data = "";
+                    data += "\nExecution Timed Out";
+                    // console.log("Timed Out: "+sandbox.folder+" "+sandbox.langName)
                     fs.readFile(sandbox.path + sandbox.folder + '/errors_unit', 'utf8', function(err2, data2)
                     {
+                        if(!data2) data2=""
 
+                        var lines = data.toString().split('*---*')
+                        data=lines[0]
+                        var time=lines[1]
 
                         console.log("this is our data2", data2, 'this is our data 2')
-                        success(data2)
+                        success(data,data2)
                     });
-
+                });
 
             }
 
--- API/app.js
+++ API/app.js
@@ -61,7 +61,7 @@ app.post('/compile',bruteforce.prevent,function(req, res)
     var folder= 'temp/' + random(10); //folder in which the temporary folder will be saved
     var path=__dirname+"/"; //current working path
     var vm_name='virtual_machine'; //name of virtual machine that we want to execute
-    var timeout_value=20;//Timeout Value, In Seconds
+    var timeout_value=40;//Timeout Value, In Seconds
 
     //details of this are present in DockerSandbox.js
     var sandboxType = new sandBox(
