c009861777b854f827f767431ca1714b69a48db5
unknown <magnus01lysfjord@gmail.com>
2018-10-16 09:53:25 +0200

    sandbox

--- API/DockerSandbox.js
+++ API/DockerSandbox.js
@@ -127,7 +127,7 @@ DockerSandbox.prototype.prepare = function(success)
             
         });
 
-    exec("mkdir "+ this.path+this.folder + " && cp "+this.path+"/Payload/* "+this.path+this.folder+"&& chmod 777 "+ this.path+this.folder,function(st)
+    exec("mkdir "+ this.path+this.folder + " && cp "+this.path+"/Payload/* "+this.path+this.folder+"&& chmod 777 "+ this.path+this.folder,function(st_unit)
     {
         fs.writeFile(
             sandbox.path +
