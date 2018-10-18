583167d19257a3326a071a9a1fe2c5f7ea103ce1
unknown <magnus01lysfjord@gmail.com>
2018-10-18 18:07:14 +0200

    aded

--- API/testforQ.js
+++ API/testforQ.js
@@ -4,5 +4,5 @@ var add = function(a, b) {
 
 QUnit.test("my module test", function() {
     QUnit.assert.equal(add(2, 3), 5, "add function succeeds");
-    QUnit.assert.equal(add("2", 3), 5, "add function fails");
+    QUnit.assert.equal(add(2, 3), 5, "add function fails");
 });
