ac732c35cf6d6a8467e544857c9f9471db38712c
unknown <magnus01lysfjord@gmail.com>
2018-11-01 21:42:19 +0100

    '

--- API/app.js
+++ API/app.js
@@ -62,7 +62,7 @@ app.post('/compile',bruteforce.prevent,function(req, res)
     var folder= 'temp/' + random(10); //folder in which the temporary folder will be saved
     var path=__dirname+"/"; //current working path
     var vm_name='virtual_machine'; //name of virtual machine that we want to execute
-    var timeout_value=40;//Timeout Value, In Seconds
+    var timeout_value=10;//Timeout Value, In Seconds
 
     //details of this are present in DockerSandbox.js
     var sandboxType = new sandBox(
