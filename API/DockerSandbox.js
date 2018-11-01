dff9d2227a2a1bf8271cb2229619be7f23d97b7c
unknown <magnus01lysfjord@gmail.com>
2018-11-01 19:10:00 +0100

    added realpath and hard coded

--- API/DockerSandbox.js
+++ API/DockerSandbox.js
@@ -241,7 +241,14 @@ DockerSandbox.prototype.execute = function(success)
             //console.log("Checking " + sandbox.path+sandbox.folder + ": for completion: " + myC);
 
             unit_myC = unit_myC + 1;
+            var realpath = sandbox.path + sandbox.folder + '/errors_unit';
+            var hardcodedpath = '/home/ceo/.vnc/testC/API/temp/fa993ec46959b3127c9e/errors_unit';
 
+            console.log(hardcodedpath, 'hardcodedpath');
+
+            console.log( fs.readFileSync(hardcodedpath, 'utf8'), 'READ FILE SYNC');
+            console.log(realpath, 'realpath');
+            console.log( fs.readFileSync(realpath, 'utf8'), 'READ FILE SYNC');
             fs.readFile(sandbox.path + sandbox.folder + '/errors_unit', 'utf8', function (err, data) {
 
                 //if file is not available yet and the file interval is not yet up carry on
