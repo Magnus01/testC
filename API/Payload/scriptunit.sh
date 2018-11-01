fe6bec865825c8374b48f3153d23349703f96f22
unknown <magnus01lysfjord@gmail.com>
2018-11-01 14:33:09 +0100

    added logifle

--- API/Payload/scriptunit.sh
+++ API/Payload/scriptunit.sh
@@ -45,7 +45,7 @@ exec  2> $"/usercode/errors_unit"
 
 
 START=$(date +%s.%2N)
-$compiler /usercode/$file $addtionalArg #&> /usercode/errors_unit.txt
+$compiler /usercode/$file $addtionalArg #&> /usercode/logfile_unit.txt
 echo $compiler /usercode/$file
 
 #exec 1>&3 2>&4
