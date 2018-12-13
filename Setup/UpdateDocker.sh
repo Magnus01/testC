c7c931fb2557130a9e7791f49fcbb7615d25d48f
Magnus01 <magnus01lysfjord@gmail.com>
2018-12-13 14:39:29 +0100

    '

--- Setup/UpdateDocker.sh
+++ Setup/UpdateDocker.sh
@@ -2,5 +2,6 @@
 echo "Creating Docker Image"
 docker build -t 'python' - < Dockerfile
 docker build -t 'javascript' - < DockerfileReactJS
+docker pull davidjeddy/docker_puppeteer_jest
 echo "Retrieving Installed Docker Images"
 docker images
